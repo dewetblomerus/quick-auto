9d936d17944a5109fed546431b062de08d940fd1
De Wet Blomerus <dewet.blomerus@salesloft.com>
2023-04-28 13:59:55 -0400

    prettier

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,35 +1,43 @@
-import { test, expect } from "@playwright/test";
+import { test, expect } from '@playwright/test';
 
-test("has title", async ({ page }) => {
-  await page.goto("https://quickaverage.com/automate");
+test('has title', async ({ page }) => {
+  await page.goto('https://quickaverage.com/automate');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/QuickAverage/);
 });
 
-test.describe("Input form", () => {
-  test("renders form", async ({ page }) => {
-    page.goto("https://quickaverage.com/automate");
+test.describe('Input form', () => {
+  test('renders form', async ({ page }) => {
+    page.goto('https://quickaverage.com/automate');
 
-    await expect(page.getByLabel("Name")).toBeVisible();
-    await expect(page.getByLabel("Number")).toBeVisible();
-    await expect(page.getByLabel("Only Viewing")).toBeVisible();
+    await expect(page.getByLabel('Name')).toBeVisible();
+    await expect(page.getByLabel('Number')).toBeVisible();
+    await expect(page.getByLabel('Only Viewing')).toBeVisible();
   });
 
-  test("hides number input when only viewing", async ({ page }) => {
-    await page.goto("https://quickaverage.com/automate");
+  test('hides number input when only viewing', async ({ page }) => {
+    await page.goto('https://quickaverage.com/automate');
 
-    page.on("websocket", (ws) => {
+    page.on('websocket', (ws) => {
       // ws.on("framesent", (event) => console.log(JSON.parse(event.payload)));
       expect(ws.isClosed()).toBeFalsy();
     });
 
     await new Promise((r) => setTimeout(r, 100));
 
-    await page.getByLabel("Only Viewing").check();
+    await page.getByLabel('Only Viewing').check();
 
-    expect(await page.getByLabel("Only Viewing").isChecked()).toBeTruthy();
+    expect(await page.getByLabel('Only Viewing').isChecked()).toBeTruthy();
 
-    await expect(page.getByLabel("Number")).not.toBeVisible();
+    await expect(page.getByLabel('Number')).not.toBeVisible();
+  });
+});
+
+test.describe('User name', () => {
+  test('renders a list of all the users names', async ({ page }) => {
+    await page.goto('https://quickaverage.com/automate');
+
+    await page.getByLabel('Name');
   });
 });
