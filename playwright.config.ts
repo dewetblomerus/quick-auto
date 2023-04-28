5a437a13c72608cdcccb6f434e813dada1a0b15c
De Wet Blomerus <dewet.blomerus@salesloft.com>
2023-04-28 15:45:17 -0400

    test basic functionality

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,10 +21,14 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'list',
+  timeout: 5000,
+  expect: {
+    timeout: 2000,
+  },
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: 'https://quickaverage.com',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
---
+++ tests/average.spec.ts
@@ -0,0 +1,95 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('/automate');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/QuickAverage/);
+});
+
+test.describe('Initial Page Load', () => {
+  test('renders form', async ({ page }) => {
+    page.goto('https://quickaverage.com/automate');
+
+    await expect(page.getByLabel('Name')).toBeVisible();
+    await expect(page.getByLabel('Number')).toBeVisible();
+    await expect(page.getByLabel('Only Viewing')).toBeVisible();
+  });
+
+  test('hides number input when only viewing', async ({ page }) => {
+    await page.goto('/automate');
+
+    page.on('websocket', (ws) => {
+      // ws.on("framesent", (event) => console.log(JSON.parse(event.payload)));
+      expect(ws.isClosed()).toBeFalsy();
+    });
+
+    await new Promise((r) => setTimeout(r, 100));
+
+    await page.getByLabel('Only Viewing').check();
+
+    expect(await page.getByLabel('Only Viewing').isChecked()).toBeTruthy();
+
+    await expect(page.getByLabel('Number')).not.toBeVisible();
+  });
+
+  test('only the first users sees admin buttons', async ({ browser }) => {
+    // Create two isolated browser contexts
+    const firstContext = await browser.newContext();
+    const secondContext = await browser.newContext();
+
+    // Create pages and interact with contexts independently
+    const firstPage = await firstContext.newPage();
+    const secondPage = await secondContext.newPage();
+
+    await firstPage.goto('/');
+    const firstUrl = firstPage.url();
+
+    // url ends in numbers
+    expect(firstUrl).toMatch(/\d+$/);
+    await secondPage.goto(firstUrl);
+
+    await expect(
+      firstPage.getByRole('button', { name: 'Clear Numbers' })
+    ).toBeVisible();
+
+    await expect(
+      secondPage.getByRole('button', { name: 'Clear Numbers' })
+    ).not.toBeVisible();
+  });
+});
+
+test.describe('User list', () => {
+  test('renders a list of all the users input and the average', async ({
+    browser,
+  }) => {
+    // Create two isolated browser contexts
+    const firstContext = await browser.newContext();
+    const secondContext = await browser.newContext();
+
+    // Create pages and interact with contexts independently
+    const firstPage = await firstContext.newPage();
+    const secondPage = await secondContext.newPage();
+
+    await firstPage.goto('/');
+    const firstUrl = firstPage.url();
+
+    // url ends in numbers
+    expect(firstUrl).toMatch(/\d+$/);
+    await secondPage.goto(firstUrl);
+    await new Promise((r) => setTimeout(r, 150));
+
+    await firstPage.getByLabel('Name').fill('Peter');
+    await firstPage.getByLabel('Number').fill('7');
+
+    await secondPage.getByLabel('Name').fill('Suzie');
+    await secondPage.getByLabel('Number').fill('10');
+
+    await expect(secondPage.getByText('Peter')).toBeVisible();
+    await expect(secondPage.getByText('7')).toBeVisible();
+    await expect(firstPage.getByText('Suzie')).toBeVisible();
+    await expect(firstPage.getByText('10')).toBeVisible();
+    await expect(firstPage.getByText('Average:')).toBeVisible();
+    await expect(firstPage.getByText('8.5')).toBeVisible();
+  });
+});
