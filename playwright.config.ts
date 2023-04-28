291383aab1a84a7905bd360ca9e2b99eafcfb756
De Wet Blomerus <dewet.blomerus@salesloft.com>
2023-04-28 16:19:18 -0400

    Move regularly changing config to the top

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,6 +10,14 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    baseURL: 'https://quickaverage.com',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+  },
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -25,15 +33,6 @@ export default defineConfig({
   expect: {
     timeout: 2000,
   },
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://quickaverage.com',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
-  },
-
   /* Configure projects for major browsers */
   projects: [
     {
